fdfe778934afd821c80f4e9f59f18b5d52158350
lmcosta10 <letmc14@gmail.com>
2024-01-08 22:25:45 -0300

    feat: nav bar fade

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 const tabs = document.querySelectorAll(".careers-tab");
 const tabsContainer = document.querySelector(".careers-tab-container");
 const tabsContent = document.querySelectorAll(".careers-content");
+const nav = document.querySelector(".nav");
 
 // Smooth scroll behavior for nav bar
 document.querySelector(".nav-links").addEventListener("click", function (e) {
@@ -28,3 +29,19 @@ tabsContainer.addEventListener("click", function (e) {
     .querySelector(`.careers-content-${clicked.dataset.tab}`)
     .classList.add("careers-content-active");
 });
+
+// Nav bar fade
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav-link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav-link");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+  }
+};
+
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+
+nav.addEventListener("mouseout", handleHover.bind(1));
